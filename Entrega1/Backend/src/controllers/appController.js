360d09565f4e32c216e13b1018aa7ff36d5e3da4
Katiê Prado <katiekpg@gmail.com>
2026-09-25 19:50:01 -0300

    Update appController.js

--- Entrega1/Backend/src/controllers/appController.js
+++ Entrega1/Backend/src/controllers/appController.js
@@ -1,3 +1,12 @@
-exports.getAppInfo = async (req, res) => {
-    return res.status(200).json({ message: "Informações gerais do aplicativo." });
+const getAppInfo = async (req, res) => {
+    return res.status(200).json({ success: true, message: "Informações gerais do aplicativo." });
+};
+
+const getAppStatus = async (req, res) => {
+    return res.status(200).json({ success: true, message: "Status do sistema operacional." });
+};
+
+module.exports = {
+    getAppInfo,
+    getAppStatus
 };
